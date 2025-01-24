acabdbd9e35399a9b2c23ca2cbb1cd9b8b787f52
salodoxera <DavidDubovik19@gmail.com>
2025-01-24 16:16:04 +0200

    fix 0.5

--- task.sh
+++ task.sh
@@ -3,8 +3,8 @@
 DB_USER=$DB_USER
 DB_PASSWORD=$DB_PASSWORD
 
-mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --result-file=backup.sql && \
-mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < backup.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-db --result-file=backup.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < backup.sql
 
-mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --no-create-db --result-file=backup-no-create-db.sql && \
-mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment < backup-no-create-db.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-db --no-create-info --result-file=backup-no-create-db.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < backup-no-create-db.sql
